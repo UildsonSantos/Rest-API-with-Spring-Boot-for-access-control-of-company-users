58f6190ee81371106463ff23ff6274353497b39f
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-06-27 11:12:44 -0300

    created a new workspace and initial project settings

---
+++ src/main/java/com/dio/santander/bootcamp/uss/UssApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bootcamp.uss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UssApplication.class, args);
+	}
+
+}
